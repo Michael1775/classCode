713258cecf7dfa44829565d8d6a4e97acd7883cf
michael1775 <michael.cuevas.17@wheels-student.org>
2016-03-10 21:58:59 +0000

    PRINT BEAR

--- forLoops/project/script.js
+++ forLoops/project/script.js
@@ -1,2 +1,12 @@
 $( document ).ready(function() {
+    while(count == 0)
+    $('body').append('<img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Ursus_arctos_-_Norway.jpg" />'); 
+
+for (var count = 0; count < 100; count ++) {
+
+    $('body').append('<img src="http://www.defenders.org/sites/default/files/styles/large/public/grizzly-bear-harry-bosen-dpc.jpg" />')
+    $('body').append('<img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Ursus_arctos_-_Norway.jpg" />'); 
+
+}
+
 });
